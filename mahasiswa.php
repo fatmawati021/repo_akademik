53f5d9a6f2730c6812b3a5ab86442ddc9e144a27
fatmawati021 <fmawati107@gmail.com>
2025-03-10 11:47:10 +0700

    Create mahasiswa.php
    Buat modul mahasiswa

---
+++ mahasiswa.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Data Mahasiswa</title>
+    </head>
+    <body>
+        <h6>Data Mahasiswa Amp</h6>
+        <hr>
+        <h6>NIM : 23110011</h6><br>
+        <h6>Nama : Fatmawati</h6><br>
+        <a href="index.php">Kembali</a>
+    </body>
+</html>
